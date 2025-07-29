30d31d7c13a7cedc55b967d08c0758084eef9ac0
aniruth6k <aniruthprabug@gmail.com>
2025-07-29 23:21:27 +0530

    Added Week 6 Hands-on

---
+++ Week6_React/6_1.ReactJS-HOL/README.txt
@@ -0,0 +1,6 @@
+My First React App
+
+How to Run
+
+1. npm install
+2. npm run dev
---
+++ Week6_React/6_1.ReactJS-HOL/code/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h2>Welcome to the first session of React</h2>
+    </>
+  )
+}
+
+export default App
